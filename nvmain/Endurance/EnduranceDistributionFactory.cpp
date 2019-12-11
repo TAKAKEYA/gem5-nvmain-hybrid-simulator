ebf5280e3aa575536c262e0be507558c896442f4
TAKAKEYA <43529975+TAKAKEYA@users.noreply.github.com>
2019-12-11 16:52:12 +0800

    Update EnduranceDistributionFactory.cpp

--- nvmain/Endurance/EnduranceDistributionFactory.cpp
+++ nvmain/Endurance/EnduranceDistributionFactory.cpp
@@ -11,3 +11,28 @@
 
 using namespace NVM;
 
+EnduranceDistribution *EnduranceDistributionFactory::CreateEnduranceDistribution( 
+        std::string distName, Config *conf )
+{
+    EnduranceDistribution *enduranceDist = NULL;
+
+    if( distName == "" )
+        std::cout << "NVMain: EnduranceDist is not set in configuration file!\n";
+    
+    if( distName == "Normal" ) 
+        enduranceDist = new NormalDistribution( conf );
+    /*
+     *  Add your custom endurance distribution here, for example:
+     *
+     *  else if( distName == "MyDist" ) enduranceDist = new MyDist( );
+     */
+    if( distName == "Uniform" ) 
+        enduranceDist = new UniformDistribution( conf );
+
+
+    if( enduranceDist == NULL )
+        std::cout << "NVMain: Endurance distribution '" << distName 
+            << "' not found in factory!\n";
+
+    return enduranceDist;
+}
