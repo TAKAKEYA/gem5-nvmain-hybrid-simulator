411a2ace2e150ed5176e40677be087ed8ac74819
TAKAKEYA <43529975+TAKAKEYA@users.noreply.github.com>
2019-12-11 16:27:35 +0800

    Update Uniform.cpp

--- nvmain/Endurance/Distributions/Uniform.cpp
+++ nvmain/Endurance/Distributions/Uniform.cpp
@@ -4,24 +4,3 @@
 
 using namespace NVM;
 
-UniformDistribution::UniformDistribution( Config *conf )
-{
-    config = conf;
-
-
-    if( conf->GetValue( "EnduranceDistMean" ) == -1 )
-    {
-        std::cout << "EnduranceDistMean parameter not found for normal \
-            distribution!!\n";
-        mean = 1000000;
-    }
-    else
-    {
-        mean = conf->GetValue( "EnduranceDistMean" );
-    }
-}
-
-uint64_t UniformDistribution::GetEndurance( )
-{
-    return mean;
-}
