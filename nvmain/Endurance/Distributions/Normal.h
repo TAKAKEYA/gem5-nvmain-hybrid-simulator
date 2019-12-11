d9afbc26beeb077b258d4c3d3006e1c6b1eba4cb
TAKAKEYA <43529975+TAKAKEYA@users.noreply.github.com>
2019-12-11 16:24:59 +0800

    Update Normal.h

--- nvmain/Endurance/Distributions/Normal.h
+++ nvmain/Endurance/Distributions/Normal.h
@@ -4,4 +4,29 @@
 #include "src/EnduranceDistribution.h"
 
 namespace NVM {
+class NormalDistribution : public EnduranceDistribution
+{
+  public:
+    NormalDistribution( );
+    NormalDistribution( Config *conf );
+    ~NormalDistribution( ) { }
 
+    uint64_t GetEndurance( );
+
+    void SetMean( uint64_t m ) { mean = m; }
+    void SetVariance( uint64_t var ) { variance = var; }
+
+    uint64_t GetMean( ) { return mean; }
+    uint64_t GetVariance( ) { return variance; }
+
+  private:
+    Config *config;
+
+    uint64_t mean;
+    uint64_t variance;
+    double nextEndurance;
+};
+
+};
+
+#endif
