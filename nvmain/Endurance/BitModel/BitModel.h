1de37580ca5e1daf5de1eabb6da5254f81a23fa7
TAKAKEYA <43529975+TAKAKEYA@users.noreply.github.com>
2019-12-11 16:37:39 +0800

    Update BitModel.h

--- nvmain/Endurance/BitModel/BitModel.h
+++ nvmain/Endurance/BitModel/BitModel.h
@@ -3,19 +3,6 @@
 
 #include "src/EnduranceModel.h"
 
-namespace NVM {
 
-class BitModel : public EnduranceModel
-{
-  public:
-    BitModel( );
-    ~BitModel( );
-
-    void SetConfig( Config *config, bool createChildren = true );
-
-    ncycles_t Read( NVMainRequest *request );
-    ncycles_t Write( NVMainRequest *request, NVMDataBlock& oldData );
-};
-};
 
 #endif
