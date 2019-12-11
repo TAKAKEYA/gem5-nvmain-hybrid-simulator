1b57434047dbd659142a393aa19825c1c941d6a5
TAKAKEYA <43529975+TAKAKEYA@users.noreply.github.com>
2019-12-11 16:50:57 +0800

    Update EnduranceDistributionFactory.h

--- nvmain/Endurance/EnduranceDistributionFactory.h
+++ nvmain/Endurance/EnduranceDistributionFactory.h
@@ -5,18 +5,5 @@
 #include "src/EnduranceDistribution.h"
 #include "src/Config.h"
 
-namespace NVM {
-
-class EnduranceDistributionFactory
-{
-  public:
-    EnduranceDistributionFactory( ) { }
-    ~EnduranceDistributionFactory( ) { }
-
-    static EnduranceDistribution *CreateEnduranceDistribution( 
-            std::string distName, Config *conf );
-};
-
-};
 
 #endif
