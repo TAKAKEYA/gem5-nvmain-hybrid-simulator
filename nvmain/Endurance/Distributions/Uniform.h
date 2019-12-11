8d9bd25a5c53422da4c3f54333d24607ac7fa7aa
TAKAKEYA <43529975+TAKAKEYA@users.noreply.github.com>
2019-12-11 16:29:03 +0800

    Update Uniform.h

--- nvmain/Endurance/Distributions/Uniform.h
+++ nvmain/Endurance/Distributions/Uniform.h
@@ -3,3 +3,22 @@
 
 #include "src/EnduranceDistribution.h"
 
+namespace NVM {
+
+class UniformDistribution : public EnduranceDistribution
+{
+  public:
+    UniformDistribution( Config *conf );
+    ~UniformDistribution( ) { }
+
+    uint64_t GetEndurance( );
+
+  private:
+    Config *config;
+
+    uint64_t mean;
+};
+
+};
+
+#endif
