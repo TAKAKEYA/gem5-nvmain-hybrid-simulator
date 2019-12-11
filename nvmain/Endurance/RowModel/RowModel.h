d4a29ae6e457ee2882fe75d6a5881493bcfc14ca
TAKAKEYA <43529975+TAKAKEYA@users.noreply.github.com>
2019-12-11 16:11:53 +0800

    Update RowModel.h

--- nvmain/Endurance/RowModel/RowModel.h
+++ nvmain/Endurance/RowModel/RowModel.h
@@ -1,1 +1,23 @@
 
+#ifndef __ROWMODEL_H__
+#define __ROWMODEL_H__
+
+#include "src/EnduranceModel.h"
+
+namespace NVM {
+
+class RowModel : public EnduranceModel
+{
+  public:
+    RowModel( );
+    ~RowModel( );
+
+    void SetConfig( Config *conf, bool createChildren = true );
+
+    ncycles_t Read( NVMainRequest *request );
+    ncycles_t Write( NVMainRequest *request, NVMDataBlock& oldData );
+};
+
+};
+
+#endif
