af9d4f162fa95b5352b2ce18a6c7c08bf8b49772
TAKAKEYA <43529975+TAKAKEYA@users.noreply.github.com>
2019-12-11 16:32:57 +0800

    Update ByteModel.h

--- nvmain/Endurance/ByteModel/ByteModel.h
+++ nvmain/Endurance/ByteModel/ByteModel.h
@@ -4,5 +4,21 @@
 
 #include "src/EnduranceModel.h"
 
+namespace NVM {
+
+class ByteModel : public EnduranceModel
+{
+  public:
+    ByteModel( );
+    ~ByteModel( );
+
+    void SetConfig( Config *config, bool createChildren = true );
+
+    ncycles_t Read( NVMainRequest *request );
+    ncycles_t Write( NVMainRequest *request, NVMDataBlock& oldData );
+
+};
+
+};
 
 #endif
