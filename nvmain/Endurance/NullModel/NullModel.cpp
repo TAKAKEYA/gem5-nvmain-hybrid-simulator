decd271dbac3333d05932a85406f194e1df16f5a
TAKAKEYA <43529975+TAKAKEYA@users.noreply.github.com>
2019-12-11 16:13:30 +0800

    Update NullModel.cpp

--- nvmain/Endurance/NullModel/NullModel.cpp
+++ nvmain/Endurance/NullModel/NullModel.cpp
@@ -1,2 +1,20 @@
 #include "Endurance/NullModel/NullModel.h"
+using namespace NVM;
 
+NullModel::NullModel( )
+{
+}
+
+NullModel::~NullModel( )
+{
+}
+
+ncycles_t NullModel::Read( NVMainRequest* /*request*/ )
+{
+    return 0;
+}
+
+ncycles_t NullModel::Write( NVMainRequest* /*request*/, NVMDataBlock& /*oldData*/ ) 
+{
+    return 0;
+}
