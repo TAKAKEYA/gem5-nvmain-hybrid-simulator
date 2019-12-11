16804a66d120cc5e2eb6239d1c3c3c31bcac359b
TAKAKEYA <43529975+TAKAKEYA@users.noreply.github.com>
2019-12-11 16:48:30 +0800

    Update EnduranceModelFactory.h

--- nvmain/Endurance/EnduranceModelFactory.h
+++ nvmain/Endurance/EnduranceModelFactory.h
@@ -4,6 +4,18 @@
 
 #include "Endurance/RowModel/RowModel.h"
 
+namespace NVM {
+
+class EnduranceModelFactory
+{
+  public:
+    EnduranceModelFactory( ) {}
+    ~EnduranceModelFactory( ) {}
+
+    static EnduranceModel *CreateEnduranceModel( std::string modelName );
+};
+
+
 };
 
 #endif
